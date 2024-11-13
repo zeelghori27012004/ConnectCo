8db5bbb79e2f3fd3b07ab777ffa1aacb2b3e7ca2
NaisargiPatel19 <202201291@daiict.ac.in>
2024-11-14 01:49:17 +0530

    Connection of MongoDB

--- react/backend/src/index.js
+++ react/backend/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express from 'express';
+import mongoose from 'mongoose';
+import Connection from './db.js';
+import dotenv from 'dotenv';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+const server = express();
+let PORT = 3000;
+
+server.listen(PORT,()=>{
+    console.log('listening on port-> '+PORT);
+})
+
+
+dotenv.config();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+Connection(USERNAME, PASSWORD);
+
