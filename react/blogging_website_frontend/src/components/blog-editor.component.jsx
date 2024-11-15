ef9a50a4a41c930eb52fa29f894f528b712fad47
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2024-11-16 04:13:09 +0530

    Frontend add event handlers in blog editor components and added features of text edior in posts and added tool components for it

--- react/blogging_website_frontend/src/components/blog-editor.component.jsx
+++ react/blogging_website_frontend/src/components/blog-editor.component.jsx
@@ -13,111 +13,158 @@ import axios from "axios";
 import { UserContext } from "../App";
 
 const BlogEditor = () => {
-    let {
-      blog,
-      blog: { title, banner, content, tags, des },
-      setBlog
-    } =  useContext(EditorContext);
+  let {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+    textEditor,
+    setTextEditor,
+    setEditorState,
+  } = useContext(EditorContext);
 
-    let { blog_id } = useParams();
+  let {
+    userAuth: { access_token },
+  } = useContext(UserContext);
 
-    let navigate = useNavigate();
+  let { blog_id } = useParams();
 
-    const handleBannerUpload = (e) => {
-        let img = e.target.files[0];
-    
-        if (img) {
-          let loadingToast = toast.loading("Uploading...");
-    
-          uploadImage(img)
-            .then((url) => {
-              if (url) {
-                toast.dismiss(loadingToast);
-                toast.success("Uploaded 👍");
-    
-                setBlog({ ...blog, banner: url });
-              }
-            })
-            .catch((err) => {
-              toast.dismiss(loadingToast);
-              return toast.error(err);
-            });
-        }
-      };
-    
-      const handleTitleKeyDown = (e) => {
-        if (e.keyCode == 13) {
-          // enter key
-          e.preventDefault();
-        }
-      };
-    
-      const handleTitleChange = (e) => {
-        let input = e.target;
-    
-        input.style.height = "auto";
-        input.style.height = input.scrollHeight + "px";
-    
-        setBlog({ ...blog, title: input.value });
-      };
-    
-      const handleError = (e) => {
-        let img = e.target;
-        img.src = defaultBanner ;
-      };
+  let navigate = useNavigate();
 
-      return (
-        <>
-          <nav className="navbar">  
-            <Link to="/" className="flex-none w-10">
-              <img src= {logo} />
-            </Link>
-            <p className="max-md:hidden text-black line-clamp-1 w-full">
-              {title.length ? title : "New Blog"}
-            </p>
-    
-            <div className="flex gap-4 ml-auto">
-              <button className="btn-dark py-2" onClick={handlePublishEvent}>
-                Publish
-              </button>
-              <button className="btn-light py-2" onClick={handleSaveDraft}>
-                Save Draft
-              </button>
+  // useEffect
+  useEffect(() => {
+    if (!textEditor.isReady) {
+      setTextEditor(
+        new EditorJS({
+          holderId: "textEditor",
+          data: Array.isArray(content) ? content[0] : content,
+          tools: tools,
+          placeholder: "Let's write an awesome story",
+        })
+      );
+    }
+  }, []);
+
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0];
+
+    if (img) {
+      let loadingToast = toast.loading("Uploading...");
+
+      uploadImage(img)
+        .then((url) => {
+          if (url) {
+            toast.dismiss(loadingToast);
+            toast.success("Uploaded 👍");
+
+            setBlog({ ...blog, banner: url });
+          }
+        })
+        .catch((err) => {
+          toast.dismiss(loadingToast);
+          return toast.error(err);
+        });
+    }
+  };
+
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      // enter key
+      e.preventDefault();
+    }
+  };
+
+  const handleTitleChange = (e) => {
+    let input = e.target;
+
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+
+    setBlog({ ...blog, title: input.value });
+  };
+
+  const handleError = (e) => {
+    let img = e.target;
+    img.src = defaultBanner;
+  };
+
+  const handlePublishEvent = () => {
+    if (!banner.length) {
+      return toast.error("Upload a blog banner to publish it");
+    }
+
+    if (!title.length) {
+      return toast.error("Write blog title to publish it");
+    }
+
+    if (textEditor.isReady) {
+      textEditor
+        .save()
+        .then((data) => {
+          if (data.blocks.length) {
+            setBlog({ ...blog, content: data });
+            setEditorState("publish");
+          } else {
+            return toast.error("Write something in your blog to publish it");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} />
+        </Link>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">
+          {title.length ? title : "New Blog"}
+        </p>
+
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2" onClick={handlePublishEvent}>
+            Publish
+          </button>
+          <button className="btn-light py-2" onClick={handleSaveDraft}>
+            Save Draft
+          </button>
+        </div>
+      </nav>
+      <Toaster />
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                <img src={banner} className="z-20" onError={handleError} />
+                <input
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
             </div>
-          </nav>
-          <Toaster />
-          <AnimationWrapper>
-            <section>
-              <div className="mx-auto max-w-[900px] w-full">
-                <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
-                  <label htmlFor="uploadBanner">
-                    <img src={banner} className="z-20" onError={handleError} />
-                    <input
-                      id="uploadBanner"
-                      type="file"
-                      accept=".png, .jpg, .jpeg"
-                      hidden
-                      onChange={handleBannerUpload}
-                    />
-                  </label>
-                </div>
-    
-                <textarea
-                  defaultValue={title}
-                  placeholder="Blog Title"
-                  className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40 bg-white"
-                  onKeyDown={handleTitleKeyDown}
-                  onChange={handleTitleChange}
-                ></textarea>
-    
-                <hr className="w-full opacity-10 my-5" />
-    
-                <div id="textEditor" className="font-gelasio"></div>
-              </div>
-            </section>
-          </AnimationWrapper>
-        </>
-      );   
-}
+
+            <textarea
+              defaultValue={title}
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40 bg-white"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
+
+            <hr className="w-full opacity-10 my-5" />
+
+            <div id="textEditor" className="font-gelasio"></div>
+          </div>
+        </section>
+      </AnimationWrapper>
+    </>
+  );
+};
 
 export default BlogEditor;
