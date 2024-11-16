e169216ec36ba0a7746f1cbbe13a9064e9397547
Heman Chauhan <hemanchauhan1874@gmail.com>
2024-11-16 12:08:43 +0530

    Backend Latest Blog query from database

--- react/server/server.js
+++ react/server/server.js
@@ -220,6 +220,27 @@ const verifyJWT = (req, res, next) => {
     })
 }
 
+server.post('/latest-blogs', (req, res) => {
+
+    let { page } = req.body;
+
+    let maxLimit = 5;
+
+    Blog.find({ draft: false })
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({ "publishedAt": -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then(blogs => {
+        return res.status(200).json({ blogs })
+    })
+    .catch(err => {
+        return res.status(500).json({ error: err.message })
+    })
+
+})
+
 server.post('/create-blog', verifyJWT, (request, response) => {
 
     let authorId = request.user;
