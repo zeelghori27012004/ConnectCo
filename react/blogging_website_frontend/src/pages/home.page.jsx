db548c6bcc0933c66fd465e6f66a61e3a8ea7dda
Pritish-Desai <desaipritish4@gmail.com>
2024-11-16 23:57:42 +0530

    Frontend HomePage-InPageNavigation

--- react/blogging_website_frontend/src/pages/home.page.jsx
+++ react/blogging_website_frontend/src/pages/home.page.jsx
@@ -1,8 +1,19 @@
 // import React from 'react'
+import axios from 'axios';
 import AnimationWrapper from '../common/page-animation';
 import InPageNavigation from '../components/inpage-navigation.component';
 
 const HomePage = () => {
+    
+    const fetchLatestBlogs = () => {
+        axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+        .then(({blogs}) => {
+            console.log(blogs);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
   return (
     <AnimationWrapper>
         <section className='h-cover flex justify-center gap-10'>
