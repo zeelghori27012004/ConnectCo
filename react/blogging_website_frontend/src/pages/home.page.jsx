b138ff9f77d5564dda868fd19187e54a3b9fe697
Pritish-Desai <desaipritish4@gmail.com>
2024-11-16 23:37:17 +0530

    Frontend HomePage-InPageNavigation Updated

---
+++ react/blogging_website_frontend/src/pages/home.page.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+import AnimationWrapper from '../common/page-animation';
+import InPageNavigation from '../components/inpage-navigation.component';
+
+const HomePage = () => {
+  return (
+    <AnimationWrapper>
+        <section className='h-cover flex justify-center gap-10'>
+            {/* latest blogs  */}
+            <div className='w-full'>
+                <InPageNavigation routes={["home","trending blogs"]} defaultHidden={["trending blogs"]}>
+                    <h1>Latest Blogs here</h1>
+                    <h1>Trending Blogs here</h1>
+                </InPageNavigation>
+            </div>
+            {/* filters and trending blogs */}
+            <div>
+
+            </div>
+        </section>
+    </AnimationWrapper>
+  )
+}
+
+export default HomePage;
