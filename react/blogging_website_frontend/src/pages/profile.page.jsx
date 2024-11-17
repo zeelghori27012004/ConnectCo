7d43d7ca1f9cd2178d28437d2572955922156b5e
Aditya Iyer <crn7forlife@gmail.com>
2024-11-17 16:14:41 +0530

    frontend 404

--- react/blogging_website_frontend/src/pages/profile.page.jsx
+++ react/blogging_website_frontend/src/pages/profile.page.jsx
@@ -21,29 +21,24 @@ export const profileDataStructure = {
     },
     account_info: {
         total_posts: 0,
-        total_reads: 0
+        total_blogs: 0
     },
-    social_links: {},
+    social_links: { },
     joinedAt: " "
-};
+}
 
 const ProfilePage = () => {
+
     let { id: profileId } = useParams();
 
-    let [profile, setProfile] = useState(profileDataStructure);
-    let [loading, setLoading] = useState(true);
+    let [ profile, setProfile ] = useState(profileDataStructure);
+    let [ loading, setLoading ] = useState(true);
     let [ blogs, setBlogs ] = useState(null);
     let [ profileLoaded, setProfileLoaded ] = useState("");
 
-    
-    let {
-        personal_info: { fullname, username: profile_username, profile_img, bio },
-        account_info: { total_posts, total_reads },
-        social_links,
-        joinedAt
-    } = profile;
+    let { personal_info: { fullname, username: profile_username, profile_img, bio }, account_info: { total_posts, total_reads }, social_links, joinedAt } = profile;
 
-    let { userAuth: { username } } = useContext(UserContext);
+    let { userAuth: { username } } = useContext(UserContext)
 
     const fetchUserProfile = () => {
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-profile", { username: profileId })
@@ -86,8 +81,6 @@ const ProfilePage = () => {
 
     }
 
-
-
     useEffect(() => {
 
         if(profileId != profileLoaded){
@@ -107,31 +100,81 @@ const ProfilePage = () => {
         setProfileLoaded("");
     }
 
-
     return (
+
         <AnimationWrapper>
             {
                 loading ? <Loader /> : 
-                    <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12">
-                        <div className="flex flex-col max-md:items-center gap-5 min-w-[250px] md:w-[50%] md:pl-8 md:border-l border-grey md:sticky md:top-[100px] md:py-10">
-                            <img src={profile_img} className="w-48 h-48 bg-grey rounded-full md:w-32 md:h-32" />
-                            <h1 className="text-2xl font-medium">@{profile_username}</h1>
-                            <p className="text-xl capitalize h-6">{fullname}</p>
-                            <p>{total_posts.toLocaleString()} Blogs - {total_reads.toLocaleString()} Reads</p>
-                            <div className="flex gap-4 mt-2">
-                                {
-                                    profileId === username ?
-                                    <Link to="/settings/edit-profile" className="btn-light rounded-md">Edit Profile</Link>
-                                    : " "
-                                }
-                            </div>
-                            <AboutUser className="max-md:hidden" bio={bio} social_links={social_links} joinedAt={joinedAt} />
+                    profile_username.length ?
+                        <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12">
+                            <div className="flex flex-col max-md:items-center gap-5 min-w-[250px] md:w-[50%] md:pl-8 md:border-l border-grey md:sticky md:top-[100px] md:py-10">
 
-                        </div>
-                    </section>
+                                <img src={profile_img} className="w-48 h-48 bg-grey rounded-full md:w-32 md:h-32" />
+
+                                <h1 className="text-2xl font-medium">@
+                                {profile_username}</h1>
+                                <p className="text-xl capitalize h-6">{fullname}</p>
+
+                                <p>{total_posts.toLocaleString()} Blogs - {total_reads.toLocaleString()} Reads</p>
+
+                                <div className="flex gap-4 mt-2">
+                                    {
+                                        profileId == username ?
+                                        <Link to="/settings/edit-profile" className="btn-light rounded-md">Edit Profile</Link>
+                                        : " "
+                                    }
+                                </div>
+
+                                <AboutUser className="max-md:hidden" bio={bio} social_links={social_links} joinedAt={joinedAt} />
+
+                            </div>
+
+                            <div className="max-md:mt-12 w-full">
+
+                            <InPageNavigation
+                                    routes={[ "Blogs Published" , "About"]}
+                                    defaultHidden={["About"]}
+                                >
+                                    <>
+                                        {blogs == null ? (
+                                            <Loader />
+                                        ) : (
+                                            blogs.results.length ? 
+                                                blogs.results.map((blog, i) => {
+                                                    return (
+                                                        <AnimationWrapper
+                                                            transition={{
+                                                                duration: 1,
+                                                                delay: i * 0.1,
+                                                            }}
+                                                            key={i}
+                                                        >
+                                                            <BlogPostCard
+                                                                content={blog}
+                                                                author={
+                                                                    blog.author.personal_info
+                                                                }
+                                                            />
+                                                        </AnimationWrapper>
+                                                    );
+                                                })
+                                            : <NoDataMessage message="No blogs published" />
+                                        )}
+                                        <LoadMoreDataBtn state={blogs} fetchDataFun={getBlogs} />
+                                    </>
+
+                                    <AboutUser bio={bio} social_links={social_links} joinedAt={joinedAt} />
+
+                                </InPageNavigation>
+
+                            </div>
+
+                        </section>
+                    : <PageNotFound />
             }
         </AnimationWrapper>
-    );
-};
+
+    )
+}
 
 export default ProfilePage;
