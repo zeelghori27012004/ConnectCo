22b06193a05f75ec9b81ac37b7ef3ce78af02c1f
Kritarth Joshi <kritarth.dpr@gmail.com>
2024-11-18 18:12:23 +0530

    frontend added theme in navbar,blog-editor,index.css files and added an image for themes

--- react/blogging_website_frontend/src/components/load-more.component.jsx
+++ react/blogging_website_frontend/src/components/load-more.component.jsx
@@ -3,7 +3,7 @@ const LoadMoreDataBtn = ({ state, fetchDataFun, additionalParam }) => {
     if(state != null && state.totalDocs > state.results.length){
         return (
             <button
-            onClick={() => fetchDataFun({page: state.page + 1 })}
+            onClick={() => fetchDataFun({ ...additionalParam , page: state.page + 1 })}
             className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
             >
                 Load More
