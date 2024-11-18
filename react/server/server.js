90f108e02f9c2b8f61e1bf7cb39b529efede0cbe
Siddhant Gupta <siddhant08062004@gmail.com>
2024-11-18 23:17:33 +0530

    Backend Contoller for notification

--- react/server/server.js
+++ react/server/server.js
@@ -901,6 +901,39 @@ server.get("/new-notification", verifyJWT,(req,res)=>{
     })
 })
 
+server.post("/notifications", verifyJWT,(req,res)=>{
+    let user_id= req.user;
+    let{page,filter,deletedDocCount}=req.body;
+    let maxLimit=10;
+    let findQuery= {notification_for: user_id, user: {$ne: user_id}};
+    let skipDocs= (page-1)*maxLimit;
+    if(filter!='all'){
+        findQuery.type=filter;
+    }
+    if(deletedDocCount){
+        skipDocs -= deletedDocCount;
+    }
+    Notifications.find(findQuery)
+    .skip(skipDocs)
+    .limit(maxLimit)
+    .populate("blog","title blog_id")
+    .populate("user", "personal_info.fullname personal_info.username personal_info.profile_img")
+    .populate("comment", "comment")
+    .populate("reply", "comment")
+    .sort({ createdAt:-1})
+    .select("createdAt type seen reply")
+    .then(notifications=>{
+        Notification.updateMany(findQuery,{seen:true})
+        .skip(skipDocs)
+        .limit(maxLimit)
+        .then(()=>console.log('notification seen'));
+        return res.status(200).json({notifications});
+    })
+    .catch(err=>{
+        console.log(err.message);
+        return res.status(500).json({error: err.message});
+    })
+})
 
 server.listen(PORT, () => {
     console.log('listening on port-> ' + PORT);
