77cfa02ee3483c841b3196d1bdf8bdf3c1051726
Siddhant Gupta <siddhant08062004@gmail.com>
2024-11-18 23:10:26 +0530

    Backend controller for change password

--- react/server/server.js
+++ react/server/server.js
@@ -795,6 +795,41 @@ server.post("/delete-comment", verifyJWT, (req, res) => {
 
 })
 
+Server.post("/change-password", verifyJWT , (req,res) =>{
+    let {currentPassword, newPassword} = req.body;
+    if(!passwordRegex.test(currentPassword) || !passwordRegex.test(newPassword)){
+        return res.status(403).json({error: "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters"})
+    }
+    User.findOne({_id:req.user})
+    .then((user)=>{
+        if(user.google_auth){
+            return res.status(403).json({error: "You can't change account's password because you logged in through google"})
+        }
+
+        bcrypt.compare(currentPassword,user.personal_info.password,(err,result)=>{
+            if(err){
+                return res.status(500).json({error:" some error occured while changing the password, please try again later"})
+            }
+            if(!result){
+                return res.status(403).json({error:"Incorrect current password"})
+            }
+            bcrypt.hash(newPassword, 10, (err, hashed_password)=>{
+                user.findOneAndUpdate({_id: req.User},{"personal_info.password":hashed_password})
+                .then((u)=>{
+                    return res.status(200).json({status: 'password changed'})
+                })
+                .catch(err => {
+                    return res.status(500).json({error:'Some error occured while saving new password, please try again later'})
+                })
+            })
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({error:"user not found"})
+    })
+})
+
 
 server.listen(PORT, () => {
     console.log('listening on port-> ' + PORT);
