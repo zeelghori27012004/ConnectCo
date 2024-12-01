b33542c1ac8f260d3be400c39a12dc76e00f6ac4
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2024-12-01 23:56:19 +0530

    Update main.jsx

--- react/blogging_website_frontend/src/main.jsx
+++ react/blogging_website_frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 
-
+console.log("Hi from Frontend");
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> 
     <BrowserRouter>
